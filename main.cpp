529f6dd6573fc90289ff9e9de608eadc5f0a6960
NRA1 <NRA1adamt@gmail.com>
2026-02-24 11:49:22 +0100

    Implementing help page

--- main.cpp
+++ main.cpp
@@ -10,13 +10,15 @@
 #include "Support/FileManagement.hpp"
 #include "Support/Guard.hpp"
 
+void printHelpPage(const char* program_name);
+
 int main(const int argc, char* argv[])
 {
     Error::setPhase(ErrorPhase::Preparation);
 
     if (argc == 2 && (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0))
     {
-        //TODO: print help page
+        printHelpPage(argv[0]);
         return 0;
     }
 
@@ -72,4 +74,16 @@ int main(const int argc, char* argv[])
     writeSuccessStatusFile(times);
 
     return 0;
+}
+
+void printHelpPage(const char* program_name)
+{
+    std::cout << "Usage: " << program_name << " <project_name> <algorithm_name> <io_filename_root> <times_to_execute>"
+                                              " [-dr <algator_data_root_path>] [-dl <algator_data_local_path>]\n";
+    std::cout << std::setw(28) << std::left << "<project_name>" << "Name of project to execute\n";
+    std::cout << std::setw(28) << std::left << "<algorithm_name>" << "Name of algorithm to execute\n";
+    std::cout << std::setw(28) << std::left << "<io_filename_root>" << "File path root to test input file\n";
+    std::cout << std::setw(28) << std::left << "<times_to_execute>" << "Number of times to run the algorithm\n";
+    std::cout << std::setw(28) << std::left << "<algator_data_root_path>" << "Path to \"data_root\" folder to use. If not set defaults to $ALGATOR_ROOT/data_root\n";
+    std::cout << std::setw(28) << std::left << "<algator_data_local_path>" << "Path to \"data_local\" folder to use. If not set defaults to $ALGATOR_ROOT/data_local\n";
 }
